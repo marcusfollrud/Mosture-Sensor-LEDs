47684679c93e1da51ba95950bbb58ab1a0e4ebdf
Marcus Follrud <marcus.follrud@gmail.com>
2016-05-25 15:02:06 -0400

    Update Moisture_Sensor_LEDs.ino
    minor corrections

--- Moisture_Sensor_LEDs/Moisture_Sensor_LEDs.ino
+++ Moisture_Sensor_LEDs/Moisture_Sensor_LEDs.ino
@@ -1,30 +1,26 @@
 /* 
-
 Innovativetom.com
 Flower Pot Soil Mosture Sensor
-
 A0 - Soil Mosture Sensor
 D2:D6 - LEDS 1,2,3,4,5
-
 LED1 - Green
 LED2 - Green
 LED3 - Green
 LED4 - YELLOW
 LED5 - RED
-
 Connect the Soil Mosture Sensor to anolog input pin 0, 
 and your 5 led to digital out 2-6
-
 //Adding logic for starting / stopping an motor for watering the plansts.
-
-
-
 */
 int led1 = 2;
 int led2 = 3;
 int led3 = 4;
 int led4 = 5;
 int led5 = 6;
+
+int repetitions = 0;
+
+int max_run = 60; //60 seconds
 
 int mostureSensor = 0;
 
@@ -49,6 +45,7 @@ void loop() {
   
  if (sensorValue >= 820)
  {
+
  digitalWrite(led1, HIGH);
  digitalWrite(led2, HIGH);
  digitalWrite(led3, HIGH);
@@ -69,16 +66,22 @@ else if (sensorValue >= 410 && sensorValue < 615)
  digitalWrite(led2, HIGH);
  digitalWrite(led3, LOW);
  digitalWrite(led4, LOW);
- digitalWrite(led5, LOW);
+ digitalWrite(led5, LOW); 
 }    
+///We're dry!!!
 else if (sensorValue >= 250 && sensorValue < 410)
  {
- digitalWrite(led1, HIGH);
- digitalWrite(led2, LOW);
- digitalWrite(led3, LOW);
- digitalWrite(led4, LOW);
- digitalWrite(led5, LOW);
+  if(repetitions < max_run) {
+   digitalWrite(led1, HIGH);
+   digitalWrite(led2, LOW);
+   digitalWrite(led3, LOW);
+   digitalWrite(led4, LOW);
+   digitalWrite(led5, LOW);
+   //This counter makes sure the pump doesnt run more than 60 seconds to flood your life.
+   repetitions++;
+ }
 }
+//Don't run here. This probably means that we tried to run but it didnt help! out of water?
 else if (sensorValue >= 0 && sensorValue < 250)
  {
  digitalWrite(led1, LOW);
@@ -86,12 +89,18 @@ else if (sensorValue >= 0 && sensorValue < 250)
  digitalWrite(led3, LOW);
  digitalWrite(led4, LOW);
  digitalWrite(led5, LOW);
+
+ 
+ 
 }
-if (sensorValue >= 820){
+if (sensorValue >= 250){
   delay(30000);        // delay 300 second between reads
+ if(repetitions > 0) { //Reset water timer if needed
+   repetitions = 0;
+ }
 }
 else
 {
-  delay(1000); //Check more repeatedly to not overflow the plant
+  delay(1000); //Check more repeatedly to not overflow the plant while pump is on
 }
 }
